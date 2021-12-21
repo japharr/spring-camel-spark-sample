b321a363d61230276da495a946686f525adb7f88
Jelil Adesina <jtutoff@gmail.com>
2021-12-21 05:29:18 +0100

    initial commit

---
+++ src/main/java/com/japharr/springsparksample/SpringSparkSampleApplication.java
@@ -0,0 +1,13 @@
+package com.japharr.springsparksample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSparkSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSparkSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/japharr/springsparksample/SpringSparkSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.japharr.springsparksample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSparkSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
